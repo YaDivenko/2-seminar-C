f829c75e28cd95f44f58d59a73205c44dcccf34b
Yana <yadivenko@gmail.com>
2023-01-19 16:34:18 +0300

    1 zadacha

---
+++ 1 zadacha/Program.cs
@@ -0,0 +1,6 @@
+﻿System.Console.WriteLine("Введите трехзначное число: ");
+int a = Convert.ToInt32 (Console.ReadLine ());
+
+int secondnamber = ((a/10)%10);
+
+Console.WriteLine("{0} Вторая цифра трехзначного чиcла ", secondnamber);
